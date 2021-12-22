445119ffd5656d3aa2c3e8056585740c6467df7f
Ahmed <atomcgarry@gmail.com>
2021-12-22 17:12:38 +0000

    fixed styling on menu page

--- src/data.js
+++ src/data.js
@@ -1,17 +1,17 @@
 const data = [
   {
     name: "Starters",
-    img: "https://images.unsplash.com/photo-1621852003709-763b0b32da0b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHN0YXJ0ZXJzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    img: "https://cdn.pixabay.com/photo/2019/10/24/22/40/salad-4575524_960_720.jpg",
     products: [],
   },
   {
     name: "Mains",
-    img: "https://images.unsplash.com/photo-1615719413546-198b25453f85?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzd8fHBpenphfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    img: "https://cdn.pixabay.com/photo/2017/02/15/10/57/pizza-2068272_960_720.jpg",
     products: [],
   },
   {
     name: "Desserts",
-    img: "https://images.unsplash.com/photo-1551024506-0bccd828d307?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80",
+    img: "https://cdn.pixabay.com/photo/2016/11/22/19/31/macarons-1850216_960_720.jpg",
     products: [],
   },
 ];
--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -1,17 +1,15 @@
 <template>
-  <div class="Menu">
-    <div>
-      <div
-        v-for="(section, index) of data"
-        :title="section.name"
-        :img="section.img"
-        :key="index"
-      >
-        <div class="menu-card">
-          <img :src="getImageUrl(section)" alt="image" />
-          <div class="menu-card-text">
-            <h1 class="menu-card-title">{{ section.name }}</h1>
-          </div>
+  <div class="menu">
+    <div
+      v-for="(section, index) of data"
+      :title="section.name"
+      :img="section.img"
+      :key="index"
+    >
+      <div class="menu-card">
+        <img :src="getImageUrl(section)" alt="image" class="bg-image" />
+        <div class="menu-card-text">
+          <h1 class="menu-card-title">{{ section.name }}</h1>
         </div>
       </div>
     </div>
@@ -38,21 +36,41 @@ export default {
 </script>
 
 <style>
+.menu {
+  max-width: 1500px;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+}
 .menu-card {
   height: 400px;
   width: 100%;
   margin-bottom: 10px;
+  position: relative;
 }
 
 .menu-card-text {
   height: 100%;
   width: 100%;
-  border: 1px solid black;
+  color: black;
+  position: absolute;
+  top: 0;
+  z-index: 1000;
+  font-size: 2em;
+  color: white;
 }
 .menu-card-title {
   display: flex;
   justify-content: center;
   align-items: center;
   height: 100%;
+  
+}
+
+.bg-image {
+  z-index: -1000;
+  object-fit: cover;
+  height: 100%;
+  width: 100%;
 }
 </style>
