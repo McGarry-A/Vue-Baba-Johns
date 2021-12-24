ef66c11f5caaffba61c3df5eb08ce1b0d382f7dc
Ahmed <atomcgarry@gmail.com>
2021-12-24 13:14:24 +0000

    basic cards done

--- src/components/FoodItem.vue
+++ src/components/FoodItem.vue
@@ -1,9 +1,18 @@
 <template>
-  <div>
-    <h1>The section name is {{ sectionName }}</h1>
-    <h1>
-      The product section we're looking for is {{ products[sectionName][0] }}
-    </h1>
+  <header>
+    <h1 id="section-title">{{ sectionName }}</h1>
+  </header>
+  <div class="products-section">
+    <div
+      v-for="(product, index) in products[sectionName]"
+      :key="index"
+      class="product-card"
+    >
+      <h2>{{ product.name }}</h2>
+      <p class="price">£{{ product.price }}</p>
+      <p>{{ product.description }}</p>
+      <button>Add to Basket</button>
+    </div>
   </div>
 </template>
 
@@ -18,3 +27,53 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.products-section {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 2em;
+}
+.product-card {
+  display: flex;
+  flex-direction: column;
+  border: 1px solid #eee;
+  height: 400px;
+  width: 300px;
+  margin: 0 1em;
+  padding: 2em 1em;
+  position: relative;
+  border-radius: 10px;
+}
+
+.price {
+  display: flex;
+  justify-content: end;
+  font-weight: lighter;
+  margin-bottom: 10px;
+}
+
+button {
+  display: flex;
+  justify-content: center;
+  color: white;
+  background-color: red;
+  font-weight: bold;
+  font-size: 1.1rem;
+  border: none;
+  padding: 0.5em 2em;
+  border-radius: 10px;
+  cursor: pointer;
+  position: absolute;
+  bottom: 10px;
+  left: 15%;
+}
+header {
+  display: flex;
+  justify-content: center;
+}
+#section-title {
+  margin: 2em 0;
+}
+</style>
